a9a4dc9be06543b496a9857e2e0ae13884f83ed9
Steven Chern <stevenchern@gmail.com>
2020-12-27 05:48:17 -0300

    add rspec

---
+++ spec/math_rspec.rb
@@ -0,0 +1,8 @@
+describe "math calculations" do
+  it 'does basic math' do
+    expect(9 + 2).to eq(11)
+    expect(9 * 2).to eq(18)
+    expect(9 - 2).to eq(7)
+    expect(9 / 2.0).to eq(4.5)
+  end
+end
